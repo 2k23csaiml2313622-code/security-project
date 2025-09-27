078618c3a9d7aebed7339e80b2b36f753cf5dc7c
2k23csaiml2313622-code <2k23.csaiml2313622@gmail.com>
2025-09-27 07:29:42 +0530

    data ingestion code

---
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+
+TARGET_COLUMN= "Result"
+PIPELINE_NAME: str= "NetworkSecurity"
+ARTIFACT_DIR: str= "Artifacts"
+FILE_NAME: str= "phishingData.csv"
+
+TRAIN_FILE_NAME: str= "train.csv"
+TEST_FILE_NAME: str="test.csv"
+
+
+SCHEMA_FILE_PATH= os.path.join("data_schema", "schema.yaml")
+
+
+"""data ingestion related constant start """
+
+DATA_INGESTION_COLLECTION_NAME: str= "NetworkData"
+DATA_INGESTION_DATABASE_NAME: str= "AISHAI"
+DATA_INGESTION_DIR_NAME: str= "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR :str= "feature_store"
+DATA_INGESTION_INGESTED_DIR: str= "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float= 0.2
+
+DATA_VALIDATION_DIR_NAME: str= "data_validation"
+DATA_VALIDATION_VALID_DIR: str= "validated"
+DATA_VALIDATION_INVALID_DIR: str= "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str= "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str= "report.yml"
